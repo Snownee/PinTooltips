9d8f994c34819610ff57c596e65dcbe186d4cd79
SettingDust <settingdust@gmail.com>
2024-11-04 19:35:01 +0800

    feat: ported to forge

---
+++ src/main/java/snownee/pintooltips/PinTooltipsForge.java
@@ -0,0 +1,17 @@
+package snownee.pintooltips;
+
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(PinTooltips.ID)
+public class PinTooltipsForge {
+
+	public PinTooltipsForge(FMLJavaModLoadingContext context) {
+		context.getModEventBus().addListener((FMLClientSetupEvent event) -> new PinTooltips().onInitializeClient());
+		MinecraftForge.EVENT_BUS.addListener((ScreenEvent.MouseDragged.Pre event) ->
+				PinTooltips.onDrag(event.getScreen(), event.getMouseButton(), event.getDragX(), event.getDragY()));
+	}
+}
