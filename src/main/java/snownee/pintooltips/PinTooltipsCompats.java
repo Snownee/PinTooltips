b4e6d315199eba8d4ab5d1569a5e0e250508f60e
Snownee <i@snownee.com>
2024-11-08 18:13:29 +0800

    feat: jade mod name integration

--- src/main/java/snownee/pintooltips/PinTooltipsCompats.java
+++ src/main/java/snownee/pintooltips/PinTooltipsCompats.java
@@ -1,11 +1,18 @@
 package snownee.pintooltips;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.enchantment.Enchantment;
+import snownee.jade.api.config.IWailaConfig;
+import snownee.jade.util.ModIdentification;
 import snownee.pintooltips.compat.JeedCompat;
 
 public class PinTooltipsCompats {
 	public static boolean jeed = FabricLoader.getInstance().isModLoaded("jeed");
+	public static boolean jade = FabricLoader.getInstance().isModLoaded("jade");
 
 	public static boolean canClickEffect(MobEffectInstance effectInstance) {
 		if (jeed) {
@@ -18,5 +25,35 @@ public class PinTooltipsCompats {
 		if (jeed) {
 			JeedCompat.clickEffect(effectInstance, mouseX, mouseY, button);
 		}
+	}
+
+	public static Component appendModName(Component desc, MobEffectInstance effectInstance) {
+		if (!PinTooltipsConfig.get().jadeModEnchantmentModName() || !shouldAppendModName()) {
+			return desc;
+		}
+		ResourceLocation key = BuiltInRegistries.MOB_EFFECT.getKey(effectInstance.getEffect());
+		if (key == null) {
+			return desc;
+		}
+		return appendModName(desc, ModIdentification.getModName(key));
+	}
+
+	public static Component appendModName(Component desc, Enchantment enchantment) {
+		if (!PinTooltipsConfig.get().jadeModEnchantmentModName() || !shouldAppendModName()) {
+			return desc;
+		}
+		ResourceLocation key = BuiltInRegistries.ENCHANTMENT.getKey(enchantment);
+		if (key == null) {
+			return desc;
+		}
+		return appendModName(desc, ModIdentification.getModName(key));
+	}
+
+	private static Component appendModName(Component desc, String modName) {
+		return desc.copy().append("\n").append(IWailaConfig.get().getFormatting().getModName().formatted(modName));
+	}
+
+	public static boolean shouldAppendModName() {
+		return jade && IWailaConfig.get().getGeneral().showItemModNameTooltip();
 	}
 }
--- src/main/java/snownee/pintooltips/mixin/interact/EnchantmentMixin.java
+++ src/main/java/snownee/pintooltips/mixin/interact/EnchantmentMixin.java
@@ -6,22 +6,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.enchantment.Enchantment;
 import snownee.pintooltips.PinTooltipsHooks;
-import snownee.pintooltips.util.DefaultDescriptions;
+import snownee.pintooltips.util.ComponentDecorator;
 
 @Mixin(Enchantment.class)
 public class EnchantmentMixin {
 	@ModifyReturnValue(method = "getFullname", at = @At("RETURN"))
 	private Component getFullname(final Component original, int level) {
 		if (PinTooltipsHooks.isGrabbing() && original instanceof MutableComponent component) {
-			var desc = DefaultDescriptions.forEnchantmentFormatted((Enchantment) (Object) this);
-			if (desc == null) {
-				return null;
-			}
-			component.withStyle($ -> $.withUnderlined(true).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, desc)));
+			ComponentDecorator.enchantment(component, (Enchantment) (Object) this, level);
 		}
 		return original;
 	}
