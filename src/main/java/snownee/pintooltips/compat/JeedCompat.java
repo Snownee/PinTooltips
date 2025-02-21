10392ea0818cdc67aeecb67dc71b7930083e1d89
Snownee <i@snownee.com>
2025-02-21 16:36:25 +0800

    refactor: use JEED's API

--- src/main/java/snownee/pintooltips/compat/JeedCompat.java
+++ src/main/java/snownee/pintooltips/compat/JeedCompat.java
@@ -1,25 +1,21 @@
 package snownee.pintooltips.compat;
 
-import net.mehvahdjukaar.jeed.Jeed;
+import net.mehvahdjukaar.jeed.api.JeedAPI;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.effect.MobEffectInstance;
 
 public class JeedCompat {
 	public static boolean canClickEffect(MobEffectInstance effectInstance) {
-		return isAvailable()
-				&& !BuiltInRegistries.MOB_EFFECT.getTag(Jeed.HIDDEN)
-				.map(it -> it.contains(effectInstance.getEffect()))
-				.orElse(false);
+		return isAvailable() && !JeedAPI.isEffectHidden(effectInstance.getEffect());
 	}
 
 	public static void clickEffect(MobEffectInstance effectInstance, double mouseX, double mouseY, int button) {
 		if (isAvailable()) {
-			Jeed.PLUGIN.onClickedEffect(effectInstance, mouseX, mouseY, button);
+			JeedAPI.invokeEffectClicked(effectInstance, mouseX, mouseY, button);
 		}
 	}
 
 	private static boolean isAvailable() {
-		return Jeed.PLUGIN != null && Minecraft.getInstance().level != null;
+		return Minecraft.getInstance().level != null;
 	}
 }
