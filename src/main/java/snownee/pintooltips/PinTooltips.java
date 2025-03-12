be2fd8773f0a8a9104c559d2a133b9e286777fb7
Snownee <i@snownee.com>
2025-03-12 22:38:48 +0800

    fix: add support for neoforge screen layers

--- src/main/java/snownee/pintooltips/PinTooltips.java
+++ src/main/java/snownee/pintooltips/PinTooltips.java
@@ -76,8 +76,8 @@ public class PinTooltips implements ClientModInitializer {
 
 			lastMouseMovedTime = 0;
 
-			ScreenKeyboardEvents.afterKeyPress(screen).register((ignored, key, scancode, modifiers) -> {
-				if (GRAB_KEY.matches(key, scancode)) {
+			ScreenKeyboardEvents.afterKeyPress(screen).register((screen1, key, scancode, modifiers) -> {
+				if (shouldShowTooltips(screen1) && GRAB_KEY.matches(key, scancode)) {
 					GRAB_KEY.setDown(true);
 					if (keyPressedFrames < 0) {
 						keyPressedFrames = 0;
@@ -85,14 +85,17 @@ public class PinTooltips implements ClientModInitializer {
 				}
 			});
 
-			ScreenKeyboardEvents.afterKeyRelease(screen).register((ignored, key, scancode, modifiers) -> {
-				if (GRAB_KEY.matches(key, scancode)) {
+			ScreenKeyboardEvents.afterKeyRelease(screen).register((screen1, key, scancode, modifiers) -> {
+				if (shouldShowTooltips(screen1) && GRAB_KEY.matches(key, scancode)) {
 					GRAB_KEY.setDown(false);
 					keyPressedFrames = -1;
 				}
 			});
 
-			ScreenMouseEvents.allowMouseClick(screen).register((ignored, mouseX, mouseY, button) -> {
+			ScreenMouseEvents.allowMouseClick(screen).register((screen1, mouseX, mouseY, button) -> {
+				if (!shouldShowTooltips(screen1)) {
+					return true;
+				}
 				if (button != InputConstants.MOUSE_BUTTON_LEFT && button != InputConstants.MOUSE_BUTTON_MIDDLE) {
 					return true;
 				}
@@ -114,6 +117,9 @@ public class PinTooltips implements ClientModInitializer {
 			});
 
 			ScreenMouseEvents.allowMouseRelease(screen).register((screen1, mouseX, mouseY, button) -> {
+				if (!shouldShowTooltips(screen1)) {
+					return true;
+				}
 				var focused = service.focused;
 				var dragging = service.dragging;
 				service.clearStates();
@@ -130,6 +136,9 @@ public class PinTooltips implements ClientModInitializer {
 			});
 
 			ScreenEvents.afterRender(screen).register((screen1, context, mouseX, mouseY, tickDelta) -> {
+				if (!shouldShowTooltips(screen1)) {
+					return;
+				}
 				if (hasTooltipInThisFrame) {
 					hasTooltipInThisFrame = false;
 					if (lastMouseX != mouseX || lastMouseY != mouseY) {
@@ -271,5 +280,9 @@ public class PinTooltips implements ClientModInitializer {
 	public static boolean isGrabbing() {
 		int delay = PinTooltipsConfig.hoveringAutoPinDelay;
 		return GRAB_KEY.isDown() || delay >= 0 && lastMouseMovedTime > 0 && System.currentTimeMillis() - lastMouseMovedTime >= delay;
+	}
+
+	public static boolean shouldShowTooltips(Screen screen) {
+		return Minecraft.getInstance().screen == screen;
 	}
 }
