9f65dfa0c996d17c88326b4376721338ec7b080a
SettingDust <settingdust@gmail.com>
2025-06-01 18:13:58 +0800

    feat: unpin auto pinned tooltip when click

--- src/main/java/snownee/pintooltips/PinTooltips.java
+++ src/main/java/snownee/pintooltips/PinTooltips.java
@@ -117,6 +117,9 @@ public class PinTooltips implements ClientModInitializer {
 					}
 					return false;
 				}
+				if (button == InputConstants.MOUSE_BUTTON_LEFT && service.autoPinnedTooltip() != null) {
+					service.unpin(service.autoPinnedTooltip());
+				}
 				return true;
 			});
 
